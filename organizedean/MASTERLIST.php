b0bf4f2933b596fd79f15a3990d75686427381a5
dyiroooo <132589537+dyiroooo@users.noreply.github.com>
2023-05-14 12:32:22 +0800

    Minimal UI Changes

--- organizedean/MASTERLIST.php
+++ organizedean/MASTERLIST.php
@@ -50,7 +50,7 @@ $results = mysqli_query($conn,$sql);
   <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
     <div class="d-flex align-items-center">
       <i class="fas fa-bars primary-text fs-4 me-3" id="menu-toggle"></i>
-      <h2 class="fs-2 m-0">Deans</h2>
+      <h2 class="fs-2 m-0">Dashboard</h2>
     </div>
   </nav>
 
